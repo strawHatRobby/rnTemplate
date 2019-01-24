ec185e8ef86e3e2104d4e672aeea709fb7cf53cb
robs100DaysOfCode <robfr93@icloud.com>
2019-01-25 03:45:15 +0530

    "trimmed all input, changed the color scheme"

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, TextInput, Text, ScrollView, View} from 'react-native';
+import { StyleSheet, TextInput, Text,KeyboardAvoidingView, ScrollView, View} from 'react-native';
 import { Font } from 'expo';
 import { Button } from 'react-native-elements';
 // import { ScrollView } from 'react-native-gesture-handler';
@@ -34,21 +34,24 @@ export default class App extends React.Component {
 
   addMessage = () => {
     if(this.state.textBoxValue.trim() === ""){
-      return 
+      return;
     }
     this.setState((prevState) => {
       return {
-        messages: prevState.messages.concat(prevState.textBoxValue)
+        messages: prevState.messages.concat(prevState.textBoxValue.trim()),
+        textBoxValue: ''
       };
-    });
+    }); 
   }
+
+ 
 
   renderMessages = () => {
     return(
       this.state.messages.map((message) => 
-      <View key={message} style={{height:"20%", width:"40%", backgroundColor:"#af03f5", padding: "10%", margin:"10%"}}>
-      <Text  style={{color:"#fff", fontSize:20}}>{message}</Text>
-      <Text style={{color:"#a5a5a5", fontSize:10}}>sent on {(new Date()).toString()}</Text>
+      <View key={message} style={{marginLeft:"60%", justifyContent:'center', alignItems:'center',  alignSelf:'baseline', backgroundColor:"#af03f5", padding: "1%", margin:"5%"}}>
+      <Text style={{color:"#fff", fontSize:20}}>{message}</Text>
+      <Text style={{color:"#a5a5a5", fontSize:10}}>sent on {(new Date()).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</Text>
       </View>
       )
     )
@@ -63,16 +66,16 @@ export default class App extends React.Component {
   render() {
     return (
       
-      <View style={styles.container}>
-      <ScrollView contentContainerStyle={{justifyContent:'flex-end', alignItems:'flex-end', backgroundColor:"#f00"}}>
+      <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+      <ScrollView contentContainerStyle={{ backgroundColor:"rgb(229,221,214)"}}>
           {this.renderMessages()}
       </ScrollView>
      <View style={{flexDirection:"row"}}>
-       <TextInput value={this.state.textBoxValue} onChangeText={this.changeInputBox} style={{width:"82%", paddingLeft: "2%"}} placeholder={"Enter your message"}/>
-       <Button onPress={this.addMessage} style={{width:"30%"}} title={"Send"}/>
+       <TextInput placeholderStyle={{color:'rgb(182,182,182)'}} value={this.state.textBoxValue} onChangeText={this.changeInputBox} style={{backgroundColor: '#fff', marginBottom:'2%', padding:'3%', paddingRight:'5%', borderRadius:20, borderColor:'transparent', width:"82%", paddingLeft: "2%"}} placeholder={"Enter your message"}/>
+       <Button onPress={this.addMessage} icon={{name: 'send', style: { marginRight: 0 } }} contentContainerStyle={{ textAlign:'center'}} buttonStyle={{backgroundColor:"#07B17D",marginLeft:"0%", marginRight:"2%", alignItems: 'center', justifyContent:'center', height:50, width:50, borderRadius:50}}/>
        </View>
        
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -81,7 +84,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexGrow:1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(229,221,214)',
     alignContent: 'space-between',
     flexDirection: 'column'
   },
